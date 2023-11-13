3a228bc802239d70919e40f67fcba1403f650f24
Gaz Liddon Blackbook Pro <gaz@gazaxian.com>
2023-11-13 11:16:06 +0000

    Added support for configs files in places different OSes can find them

--- src/config.rs
+++ src/config.rs
@@ -1,28 +1,41 @@
+use crate::expand::*;
 use serde::Deserialize;
 use std::path::PathBuf;
-use crate::expand::*;
 
 #[derive(Deserialize, Debug)]
 pub struct Config {
     pub repositries: Vec<PathBuf>,
 }
 
+fn find_config_files(home_dir_name: &str, non_home_dir_name: &str) -> Vec<PathBuf> {
+    let to_check = vec![
+        dirs::home_dir().map(|p| p.join(home_dir_name) ),
+        dirs::config_local_dir().map(|p| p.join(non_home_dir_name)),
+        dirs::config_dir().map(|p| p.join(non_home_dir_name))
+    ];
+
+    to_check
+        .into_iter()
+        .filter_map(std::convert::identity)
+        .filter(|p| p.exists())
+        .collect()
+}
+
 impl Config {
     pub fn new() -> Self {
-        let config = expand("~/.gs.toml");
+        let configs = find_config_files(".gs.toml", "gs.toml");
 
-        if config.exists() {
+        let config = configs.get(0);
+
+        if let Some(config) = config {
             let text = std::fs::read_to_string(config).unwrap();
             let config: Config = toml::from_str(&text).unwrap();
-            let repositries :Vec<_> = config.repositries.iter().map(expand_path).collect();
-
+            let repositries: Vec<_> = config.repositries.iter().map(expand_path).collect();
             Self {
                 repositries,
                 ..config
             }
-            
         } else {
-            println!("Can't find {:?}", config);
             Self {
                 repositries: vec![],
             }
@@ -32,6 +45,8 @@ impl Config {
 
 impl Default for Config {
     fn default() -> Self {
-        Self::new()
+        Self {
+            repositries: Default::default(),
+        }
     }
 }
